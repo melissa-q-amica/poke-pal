fe9997524668cd9c83c8df1204c4d36a64f02646
melissa quintanilla <mquintanilla@amica.com>
2025-10-14 13:52:05 -0500

    wrap app with QueryClientProvider

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,6 @@
 import { Outlet, Scripts, ScrollRestoration } from "react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "lib/query";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { theme } from "./styles/theme";
@@ -10,13 +12,17 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <title>PokéPal</title>
       </head>
       <body>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Outlet />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Outlet />
+          </ThemeProvider>
+        </QueryClientProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
