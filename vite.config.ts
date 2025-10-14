8a661dbc6567bb7fe19978352de9d94d3b72f2c8
melissa quintanilla <mquintanilla@amica.com>
2025-10-14 11:53:09 -0500

    add React Router plugin, styled-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter()],
+  resolve: {
+    alias: {
+      styles: path.resolve(__dirname, "app/styles"),
+      components: path.resolve(__dirname, "app/components"),
+      lib: path.resolve(__dirname, "app/lib"),
+    },
+  },
+  ssr: { noExternal: ["styled-components"] },
 });
